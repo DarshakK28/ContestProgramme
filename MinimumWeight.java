702a41d7c6231f7aa6093eda9941afb2ebb5f591
Darshak Kachchhi <40310380+DarshakK28@users.noreply.github.com>
2019-06-18 21:24:37 +0530

    Update MinimumWeight.java

--- MinimumWeight.java
+++ MinimumWeight.java
@@ -81,23 +81,7 @@ public class MinimumWeight {
 	                	T[i][j]=a[i][j];
 	                }
 	            }
-	        }
-	        
-	        for(int i=0; i < n; i++){
-	            for(int j=0; j < m; j++){
-	                System.out.print(a[i][j]+"   ");
-	            }
-	            System.out.println();
-	        }
-	        System.out.println();
-	        
-	        for(int i=0; i < n; i++){
-	            for(int j=0; j < m; j++){
-	                System.out.print(T[i][j]+"   ");
-	            }
-	            System.out.println();
-	        }
-	        
+	        }     
 	        return T[n-1][m-1];
 	    }
 	    
